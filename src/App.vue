ae4f9754c62d134c0c4503152abe81726f91c7f7
Nercus <s.wintersperger@me.com>
2023-09-18 17:11:23 +0200

    chore: cleanup boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>Header</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis assumenda aperiam deserunt tenetur sint ab
+      architecto in, labore illo quos eligendi alias quis? Animi fuga officiis tempora eligendi tenetur placeat.</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: $base-1;
+  color: $base-content;
 }
 </style>
