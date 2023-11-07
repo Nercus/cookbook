e3c387d8fbfa758e06f8b4cb324fb8527a356df0
Nercus <s.wintersperger@me.com>
2023-11-07 20:12:26 +0100

    chore: add 404 page

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>🫤 404</h1>
+    <h2>Page not found!</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,15 @@
 import PageHome from "../pages/PageHome.vue";
 import PageEditor from "../pages/PageEditor.vue";
+import PageNotFound from "../pages/PageNotFound.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
 
 const routes: Array<RouteRecordRaw> = [
-  { redirect: "home", path: "/" },
+  { redirect: "/", path: "/home" },
   { path: "/", component: PageHome },
   { path: "/editor", component: PageEditor },
+  { path: '/:pathMatch(.*)*', component: PageNotFound },
 ]
 
 
